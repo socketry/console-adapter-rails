6206b7c829cb5600d923cc7f845570550ee5b816
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-08-02 19:35:13 +1200

    Documentation.

--- lib/console/adapter/rails.rb
+++ lib/console/adapter/rails.rb
@@ -12,8 +12,23 @@ require 'active_job/log_subscriber'
 
 module Console
 	module Adapter
+		# A Rails adapter for the console logger.
 		module Rails
+			# A Rails log subscriber which is compatible with `Console::Logger`. It receives events from `ActiveSupport::Notifications` and logs them to the console.
 			class LogSubscriber < ActiveSupport::LogSubscriber
+				# Log controller action events.
+				#
+				# Includes the following fields:
+				# - `subject`: "process_action.action_controller"
+				# - `controller`: The name of the controller.
+				# - `action`: The action performed.
+				# - `format`: The format of the response.
+				# - `method`: The HTTP method of the request.
+				# - `path`: The path of the request.
+				# - `status`: The HTTP status code of the response.
+				# - `view_runtime`: The time spent rendering views.
+				# - `db_runtime`: The time spent querying the database.
+				# - `location`: The redirect location if any.
 				def process_action(event)
 					payload = event.payload.dup
 					
@@ -35,17 +50,20 @@ module Console
 				end
 			end
 			
-			def self.apply!(notifications: ActiveSupport::Notifications, config: ::Rails.configuration)
+			# Apply the Rails adapter to the current process and Rails application.
+			# @parameter notifications [ActiveSupport::Notifications] The notifications object to use.
+			# @parameter configuration [Rails::Configuration] The configuration object to use.
+			def self.apply!(notifications: ActiveSupport::Notifications, configuration: ::Rails.configuration)
 				if notifications
 					# Clear out all the existing subscribers otherwise you'll get double the logs:
 					notifications.notifier = ActiveSupport::Notifications::Fanout.new
 				end
 				
-				if config
+				if configuration
 					# Set the logger to a compatible logger to catch `Rails.logger` output:
-					config.logger = Console::Compatible::Logger.new("Rails", Console.logger)
+					configuration.logger = Console::Compatible::Logger.new("Rails", Console.logger)
 					# Delete `Rails::Rack::Logger` as it also doubles up on request logs:
-					config.middleware.delete ::Rails::Rack::Logger
+					configuration.middleware.delete ::Rails::Rack::Logger
 				end
 				
 				# Attach our log subscriber for action_controller events:
