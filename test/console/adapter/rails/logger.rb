26bbb5282f08e6498a458c31885964c6cd9b5ccd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-04-04 14:11:47 +1300

    Rework logger tests.

--- test/console/adapter/rails/logger.rb
+++ test/console/adapter/rails/logger.rb
@@ -9,60 +9,68 @@ require 'console/logger'
 
 describe Console::Adapter::Rails::Logger do
 	let(:capture) {Console::Capture.new}
-	let(:logger) {Console::Logger.new(capture)}
-	let(:instance) {subject.new(::Rails)}
 	
-	def before
-		super
-		Console.logger = logger
-	end
-	
-	it "should support tags" do
-		expect(Rails.logger).to be(:respond_to?, :tagged)
-	end
-	
-	it "should support silence" do
-		expect(Rails.logger).to be(:respond_to?, :silence)
+	with 'Rails.logger' do
+		def before
+			super
+			Console.logger = Console::Logger.new(capture)
+		end
 		
-		Rails.logger.silence(Logger::ERROR) do
+		it "should support tags" do
+			expect(Rails.logger).to be(:respond_to?, :tagged)
+		end
+		
+		it "should support silence" do
+			expect(Rails.logger).to be(:respond_to?, :silence)
+			
+			Rails.logger.silence(Logger::ERROR) do
+				Rails.logger.info("Hello World")
+			end
+			
+			expect(capture.last).to be_nil
+		end
+		
+		it "does not output to stdout" do
+			skip_unless_method_defined(:logger_outputs_to?, ActiveSupport::Logger.singleton_class)
+			
+			expect(
+				ActiveSupport::Logger.logger_outputs_to?(Rails.logger, STDOUT)
+			).to be == true
+		end
+		
+		it "can log a message" do
 			Rails.logger.info("Hello World")
+			
+			expect(capture.last).to have_keys(
+				severity: be == :info,
+				subject: be == Rails,
+				arguments: be == ["Hello World"]
+			)
 		end
-		
-		expect(capture.last).to be_nil
 	end
 	
-	it "does not output to stdout" do
-		skip_unless_method_defined(:logger_outputs_to?, ActiveSupport::Logger.singleton_class)
+	with '#silence' do
+		let(:logger) {subject.new(::Rails, capture)}
 		
-		expect(
-			ActiveSupport::Logger.logger_outputs_to?(Rails.logger, STDOUT)
-		).to be == true
+		it "behaves like Rails 6 logger" do
+			logger.silence(Logger::ERROR) do
+				logger.info("Hello World")
+			end
+
+			expect(capture.last).to be_nil
+		end
 	end
 	
-	it "can log a message" do
-		Rails.logger.info("Hello World")
+	with '#local_level=' do
+		let(:logger) {subject.new(::Rails, capture)}
 		
-		expect(capture.last).to have_keys(
-			severity: be == :info,
-			subject: be == Rails,
-			arguments: be == ["Hello World"]
-		)
-	end
+		it "behaves like Rails 7 logger" do
+			logger.local_level = Logger::ERROR
+			expect(logger.local_level).to be == Logger::ERROR
+			logger.info("Hello World")
+			logger.local_level = nil
 
-	it "behaves like Rails 6 logger" do
-		instance.silence(Logger::ERROR) do
-			instance.info("Hello World")
+			expect(capture.last).to be_nil
 		end
-
-		expect(capture.last).to be_nil
-	end
-
-	it "behaves like Rails 7 logger" do
-		instance.local_level = Logger::ERROR
-		expect(instance.local_level).to be == Logger::ERROR
-		instance.info("Hello World")
-		instance.local_level = nil
-
-		expect(capture.last).to be_nil
 	end
 end
